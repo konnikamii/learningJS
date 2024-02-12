c324d2759e61a5960700d623838d031ba97a8c09
konnikamii <filipcho100@gmail.com>
2024-02-12 22:21:09 +0100

    logger testing

---
+++ logger/logger.js
@@ -0,0 +1,32 @@
+console.log("This is an informational message");
+console.error("This is an error message");
+console.warn("This is a warning message");
+console.debug("This is a debug message");
+
+const winston = require("winston");
+
+// Create a logger instance with transports
+const logger = winston.createLogger({
+  // Transports define where your logs go
+  transports: [
+    // Console transport
+    new winston.transports.Console({
+      level: "debug", // Log only if info level or below
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    // File transport
+    new winston.transports.File({
+      filename: "log.log", // All logs will be saved in this file
+      level: "info",
+    }),
+  ],
+});
+
+// Log some messages
+logger.info("This is an informational message");
+logger.error("This is an error message");
+logger.warn("This is a warning message");
+logger.debug("This is a debug message");
