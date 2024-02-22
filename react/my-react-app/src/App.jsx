1a615ac878ae39f235004e4d7725bd39e908fd7e
konnikamii <filipcho100@gmail.com>
2024-02-22 12:31:15 +0100

    fix

---
+++ react/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+// App.js in React application
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Helloooo</h1>
+    </div>
+  );
+};
+
+export default App;
