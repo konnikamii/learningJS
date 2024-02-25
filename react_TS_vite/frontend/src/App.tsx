5510b08211b8bff5ea114a64e33dbafb260be0fb
konnikamii <filipcho100@gmail.com>
2024-02-25 11:45:33 +0100

    routing

--- react_TS_vite/frontend/src/App.tsx
+++ react_TS_vite/frontend/src/App.tsx
@@ -1,11 +1,33 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <ul>
+        <li>
+          <a href="/"> Home </a>
+        </li>
+        <li>
+          <a href="/features"> features </a>
+        </li>
+      </ul>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Hello</h1>
+            </div>
+          }
+        ></Route>
+        <Route path="/features">
+          <div>
+            <h1>features</h1>
+          </div>
+        </Route>
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
