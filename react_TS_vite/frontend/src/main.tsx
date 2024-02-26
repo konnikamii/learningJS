835eae3df837eb337fe947e10ae2fc09caf1a375
konnikamii <filipcho100@gmail.com>
2024-02-26 22:40:50 +0100

    new routing

--- react_TS_vite/frontend/src/main.tsx
+++ react_TS_vite/frontend/src/main.tsx
@@ -1,12 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider.tsx";
+import HeroPage from "./components/HeroPage.tsx";
+import AboutPage from "./components/AboutPage.tsx";
+import NotFoundPage from "./components/NotFoundPage.tsx";
+import FeaturesPage from "./components/FeaturesPage.tsx";
+import PricingPage from "./components/PricingPage.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HeroPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/features",
+    element: <FeaturesPage />,
+  },
+  {
+    path: "/pricing",
+    element: <PricingPage />,
+  },
+]);
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+      {/* <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter> */}
+    </React.StrictMode>
+  );
+} else {
+  console.error("Failed to find the root element");
+}
