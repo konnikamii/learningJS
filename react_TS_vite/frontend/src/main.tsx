640f86e360572f87f257c25c5797128d8724bee5
konnikamii <filipcho100@gmail.com>
2024-02-27 22:25:14 +0100

    cleanup

--- react_TS_vite/frontend/src/main.tsx
+++ react_TS_vite/frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import {
   BrowserRouter,
   Route,
@@ -9,12 +8,6 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { AuthProvider } from "./context/AuthProvider.tsx";
-import HeroPage from "./components/HeroPage.tsx";
-import AboutPage from "./components/AboutPage.tsx";
-import NotFoundPage from "./components/NotFoundPage.tsx";
-import FeaturesPage from "./components/FeaturesPage.tsx";
-import PricingPage from "./components/PricingPage.tsx";
 
 // ReactDOM.createRoot(document.getElementById('root')!).render(
 //   <React.StrictMode>
